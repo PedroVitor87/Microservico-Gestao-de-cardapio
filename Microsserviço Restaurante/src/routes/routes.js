950f4ec9804274d355c99225a9fe15082c0cc2e1
Pedro Vitor <pedrovitor812.812@gmail.com>
2024-01-10 13:24:57 -0300

    Apagando rotas que era em teste feito com array

--- Microsserviço Restaurante/src/routes/routes.js
+++ Microsserviço Restaurante/src/routes/routes.js
@@ -1,31 +1,10 @@
 const express = require('express')
 const routes = express.Router()
-const arrayPratos = require('../data/pratosArray')
 const menuController = require('../controller/menuController')
 
 routes.get("/cardapio", menuController.searchMenu);
 routes.post("/cardapio/criar-prato", menuController.createDish);
 routes.delete("/cardapio/:id/apagar-prato", menuController.deleteDishMenu);
 routes.patch("/cardapio/:id/atualizar-valor", menuController.updateDishValue);
-
-routes.get("/", (req, res) => {
-    res.status(200).json(arrayPratos)
-})
-
-routes.get("/pratos", (req, res) => {
-    res.status(200).json(arrayPratos)
-})
-
-routes.post("/pratos/novoprato", (req, res) => {
-    const {id, prato, descricao, valor} = req.body
-    const novoPrato = {
-        id,
-        prato,
-        descricao,
-        valor
-    }
-    arrayPratos.push(novoPrato)
-    res.status(201).json(novoPrato)
-})
 
 module.exports = routes;
