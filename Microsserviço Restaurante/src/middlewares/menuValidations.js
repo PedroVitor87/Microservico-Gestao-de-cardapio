9f50cde0fc2ce7e1bd6c63d05aa1ad1e69f2f826
Pedro Vitor <pedrovitor812.812@gmail.com>
2024-01-14 19:47:43 -0300

    Criação de middleware para validação de cadastro de novo prato

---
+++ Microsserviço Restaurante/src/middlewares/menuValidations.js
@@ -0,0 +1,23 @@
+const validateMenu = (req, res, next) => {
+    const { dish_name, description, value } = req.body;
+  
+    const errors = [];
+  
+    if (!dish_name || dish_name.trim() === '') {
+        errors.push('O nome do prato é obrigatório.');
+    }
+    if (description.length > 400) {
+        errors.push('A descrição não pode ter mais de 400 caracteres.');
+    }
+    if (!/^\d+(\.\d{1,2})?$/.test(value)) {
+        errors.push('O valor deve ser um número decimal com até duas casas decimais.');
+    }     
+
+    if (errors.length > 0) {
+      return res.status(400).json({ errors });
+    }
+    next();
+};
+  
+module.exports = { validateMenu };
+  
--- Microsserviço Restaurante/src/routes/routes.js
+++ Microsserviço Restaurante/src/routes/routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const routes = express.Router()
 const menuController = require('../controller/menuController')
+const { validateMenu } = require('../middlewares/menuValidations')
 
 routes.get("/cardapio", menuController.searchMenu);
-routes.post("/cardapio/criar-prato", menuController.createDish);
+routes.post("/cardapio/criar-prato", validateMenu, menuController.createDish);
 routes.delete("/cardapio/:id/apagar-prato", menuController.deleteDishMenu);
 routes.patch("/cardapio/:id/atualizar-valor", menuController.updateDishValue);
 
